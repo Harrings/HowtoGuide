5051eec7729d5f13e143a1a28f42c02eaf07c627
Harrings <sean.harrington2@gmail.com>
2015-02-24 22:01:00 -0800

    Made Set up file

--- better.php
+++ better.php
@@ -23,38 +23,92 @@
 <p>There are two major upsides to using Kint over you standard <strong>var_dump()</strong> function.
 <br>The first is the extra information it conveys to the user to help in the debugging process and the 
 <br>second is the presentation it gives to the information which makes it more accessible. </p>
-<br>
 <h3>Content</h3>
 <p>The <strong>Kint :: dump()</strong> function provides added information over the standard <strong>var_dump()</strong> function.
-<br>such as the line of code where the dump was called and where each piece of information came from. </p>
+<br>such as the line of code where the dump was called and the calling function or method. </p>
 <p>Here we have a basic function that we will call a function to push a new class onto our <strong>$courses</strong> array from the previous page
 <br>then call <strong>var_dump()</strong> then <strong>Kint :: dump()</strong> on <strong>$courses</strong> to show the difference.
 <pre>
   <code>
-	function addcourse($array) {
+	function addcourse(&$array) {
 	array_push($array, "Web Dev 2");
+	var_dump($array);
+	Kint::dump( $array);
 	}
 	
 	addcourse($courses);
-	var_dump($courses);
-	Kint::dump( $courses);
 	</code>
 </pre>
-<p>The results of the <strong>var_dump($courses)</strong> is shown below. </p>
+<p>The results of the <strong>var_dump($array)</strong> is shown below. </p>
 <?php
-	function addcourse($array) {
+	require 'Kint.class.php';
+	$courses = array(
+    161 => "Program 1A",
+    162 => "Program 1B",
+	225 => "Discrete Math",
+	290 =>"Web Development",
+	);
+	function addcourse(&$array) {
 	array_push($array, "Web Dev 2");
+
+	var_dump($array);
+?>
+<p>While the contents of the array are shown it does not display on what line the <strong>var_dump()</strong> was called or where the last value came from
+<br>however when we call <strong>Kint :: dump()</strong> we can see that the dump was called on line 60 in the function addcourse(). </p>
+<?php
+	Kint :: dump($array);
 	}
 	
 	addcourse($courses);
-	var_dump($courses);
 ?>
-<br>
-<p>While the contents of the array are shown it does not display on what line the <strong>var_dump()</strong> was called or where the last value came from
-<br>however when we call <strong>Kint :: dump()</strong> we can see this information.
+<p>While this information may seem painfully obvious in this context when working with more complex php projects with many functions/method across different
+pages being able to immediatly identify where the information came from can be very helpful </p>
+<h3>Presentation</h3>
+<p>The Kint functions display information in a much more user friendly format and allow for easier debugging and better understanding
+<br>of what is being displayed. There a myriad of situations in which the display format of <strong>Kint :: dump()</strong> is helpful
+<br>but here will cover two. First its ability organize and display data and second its ability to classify what type of data it is dealing with. </p>
+<p>Here we have a multidemsional array of cars with how many are in stock and how many have been sold. We will call <strong>var_dump($cars)</strong> then
+<br><strong>Kint :: dump($cars)</strong> to show the difference.
+<pre>
+  <code>
+  $cars = array
+  (
+  array("Volvo",22,18),
+  array("BMW",15,13),
+  array("Saab",5,2),
+  array("Land Rover",17,15)
+  );
+  </code>
+</pre>
 <?php
-	Kint::dump( $courses);
+	$cars = array
+  (
+  array("Volvo",22,18),
+  array("BMW",15,13),
+  array("Saab",5,2),
+  array("Land Rover",17,15)
+  );
+  var_dump($cars);
+  Kint::dump($cars);
+ ?>
+ <p>With <strong>var_dump()</strong> we are forced to view the entire array printed out in order however with <strong>Kint :: dump()</strong> we get a 
+<br>chart that shows the array in a format that is much easier to understand and interpret </p>
+ <p>Kint also has the ability to automatically classify what the data in array means. Here we have a string holding the number of a color
+ <br>and an intiger with a timestamp. When we call <strong>Kint :: dump()</strong> it will recognize what the data is and display it in a user
+ <br>friendly format. </p>
+ <pre>
+  <code>
+	$color="#730";
+	$info=1424827818;
+  </code>
+</pre>
+<?php
+	$color="#730";
+	$info=1424827818;
+	Kint::dump($color);
+	Kint::dump($info);
 ?>
+ 
  
 <br>
 <h3><a href="what.php"><strong>Previous</strong></a><p>            </p><a href="setup.php"><strong>Next</strong></a>
--- setup.php
+++ setup.php
@@ -14,7 +14,30 @@
 		<li><a href="tips.php">Tips and Tricks</a></li>
 	</ul>
 </nav>
-
+<h1>Guide to Basic use of Kint for PHP</h1>
+<p>by Sean Harrington</p>
+<br>
+<br>
+<div>
+<h2>Basic Setup</h2>
+<p>There is a saying that the best workout is the one you actually do and I feel this holds true for
+<br>programming tools as well. There is no point in having a great tool if the set up is so complex it
+<br>becomes difficult to use. This is where Kint shines as it is very easy to set up. You can either do an
+<br>install by hand or use composer. </p>
+<h3>Manual Install</h3>
+<p>This is the way I would recommend setting up Kint if you do not already have composer set up on your machine. </p>
+<ol>
+<li>Download Kint onto your machine either by clicking <a href="https://github.com/raveren/kint/archive/v0.9.zip">here</a> or you can navigate to the Kint website
+<br>by clicking <a href="http://raveren.github.io/kint/">here</a>.</li>
+<li>Extract/unpack the Kint folder somewhere that will be accesible by your php files. I chose to to unpack it in the same directory as my PHP files for the purpose
+<br>of this guide. </li>
+<li>Add the line <strong>require 'Kint.class.php';</strong> at the top of any files that will use the Kint functions. Since I unpacked my directory in same file 
+<br>as my PHP files no extra path is needed but if you unpacked in a different place then you would need to add the file path to the require statement. </li>
+<li>You are now set up and ready to use Kint. </li>
+</ol>
+<h3>Composer</h3>
+<li>If you do not already have Composer set up on your machine you can downloand and install it at the composer website <a href="https://getcomposer.org/">here</a></li>
+<li>Create a new file called <strong>composer.json</strong> in the directory with your php files and copy and paste the information found <a href="composer.json">here</a> into the document.
 
 
 <br>
